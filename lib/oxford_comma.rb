cf37ae065d5a615de510b8b47d9a3d4c1ff41708
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-08 02:47:05 +0300

    Done with lab test

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_element = array.pop
+    "#{array.join(", ")}, and #{last_element}"
+  end
 end
+
+puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+puts oxford_comma(["milk", "eggs"])
+puts oxford_comma(["fiddleheads"])
